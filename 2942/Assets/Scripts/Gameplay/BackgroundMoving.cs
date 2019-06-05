95f591d6e5fed705241b7f79b5daae645761253e
Franco <franco_vega@msn.com>
2019-06-04 22:58:24 -0300

    Improved sprites & added simple parallax background

---
+++ 2942/Assets/Scripts/Gameplay/BackgroundMoving.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundMoving : MonoBehaviour
+{
+    public float speed;
+
+    MeshRenderer backgroundRenderer;
+    private void Start()
+    {
+        backgroundRenderer = GetComponent<MeshRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        backgroundRenderer.material.mainTextureOffset = new Vector2(Time.time * speed, 0);
+    }
+}
