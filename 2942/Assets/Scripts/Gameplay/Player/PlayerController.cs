21299b096e0cfc888f745af0e94afc09052b58d1
Franco <franco_vega@msn.com>
2019-06-04 21:12:14 -0300

    Added sample animation & sprite

---
+++ 2942/Assets/Scripts/Gameplay/Player/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Animator playerAnimator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerAnimator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xAxis = Input.GetAxisRaw("Horizontal");
+
+        if(xAxis > 0)
+        {
+            playerAnimator.SetBool("movingRight",true);
+            playerAnimator.SetBool("movingLeft", false);
+        }
+        else if (xAxis < 0)
+        {
+            playerAnimator.SetBool("movingRight", false);
+            playerAnimator.SetBool("movingLeft", true);
+        }
+        else
+        {
+            playerAnimator.SetBool("movingRight", false);
+            playerAnimator.SetBool("movingLeft", false);
+        }
+    }
+}
