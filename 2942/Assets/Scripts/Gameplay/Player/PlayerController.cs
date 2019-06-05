98c01024089d59bd6547fd105586dc8fa4cc1c23
Franco <franco_vega@msn.com>
2019-06-04 21:47:23 -0300

    Added player movement bounds

--- 2942/Assets/Scripts/Gameplay/Player/PlayerController.cs
+++ 2942/Assets/Scripts/Gameplay/Player/PlayerController.cs
@@ -15,22 +15,42 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Vector3 pos = Camera.main.WorldToViewportPoint(transform.position);
+        pos.x = Mathf.Clamp(pos.x,0.02f,0.98f);
+        pos.y = Mathf.Clamp(pos.y, 0.08f, 0.92f);
+        transform.position = Camera.main.ViewportToWorldPoint(pos);
+        Debug.Log(pos);
+
         float xAxis = Input.GetAxisRaw("Horizontal");
+        float yAxis = Input.GetAxisRaw("Vertical");
 
         if(xAxis > 0)
         {
             playerAnimator.SetBool("movingRight",true);
             playerAnimator.SetBool("movingLeft", false);
+            transform.position = transform.position + new Vector3(1,0,0) * Time.deltaTime;
         }
         else if (xAxis < 0)
         {
             playerAnimator.SetBool("movingRight", false);
             playerAnimator.SetBool("movingLeft", true);
+            transform.position = transform.position + new Vector3(-1, 0, 0) * Time.deltaTime;
         }
         else
         {
             playerAnimator.SetBool("movingRight", false);
             playerAnimator.SetBool("movingLeft", false);
         }
+
+        if(yAxis > 0)
+        {
+            transform.position = transform.position + new Vector3(0, 1, 0) * Time.deltaTime;
+        }
+        else if (yAxis < 0)
+        {
+            transform.position = transform.position + new Vector3(0, -1, 0) * Time.deltaTime;
+        }
+
+        
     }
 }
