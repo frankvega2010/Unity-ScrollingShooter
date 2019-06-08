56de6e6730daaef5aa2b0741dd9026d028fff75b
Franco <franco_vega@msn.com>
2019-06-08 10:56:33 -0300

    Items fall down when they spawn

---
+++ 2942/Assets/Scripts/Gameplay/Item/Item.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    public float speed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = transform.position + new Vector3(0, speed, 0) * Time.deltaTime;
+    }
+}
